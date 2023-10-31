4889a841cb222e00948de55c37f1df2ec585dba7
KELVIN MURITHI <kelvinmurithi@live.com>
2023-10-31 16:11:41 +0300

    completed

--- my-app/src/components/app/App.js
+++ my-app/src/components/app/App.js
@@ -7,7 +7,7 @@ function App() {
   
   return (
     <center>
-      <div className=" h-18 bg-blue-400 ">
+      <div className=" h-18 bg-indigo-400 ">
         <p className='uppercase font-medium text-[33px]'>The Royal Bank of Flatiron</p>
       </div>
       <OutputShowing/>
